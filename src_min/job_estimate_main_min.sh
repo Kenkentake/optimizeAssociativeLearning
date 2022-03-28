a3e7ac4ff0de0d89938e774102836dbc87585045
Kenkentake <keeeentake0908@gmail.com>
2022-03-28 10:21:26 +0900

    Add minimal optimization

--- src_min/job_estimate_main_min.sh
+++ src_min/job_estimate_main_min.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
+#PJM --rsc-list "node=10"
+#PJM --rsc-list "elapse=72:00:00"
+#PJM --mpi "max-proc-per-node=8"
+#PJM --mpi "proc=1"
+#PJM -S
 
 # Make directory to save data file
 Time=`date '+%m%d%H%M%S'`
-
 RESULT_DIR="./al_result/"
 VOLTAGE_DIR="${RESULT_DIR}${Time}/voltage"
 SPIKE_DIR="${RESULT_DIR}${Time}/spike"
@@ -19,17 +23,18 @@ echo ${NRNOPT} | tee "${RESULT_DIR}${Time}/NRNOPT.param"
 NUM_OF_POP=2
 MU=1
 NUM_OF_CHILD_PROCS=2
-MAXEVAL=1
-NUM_OF_GRANDCHILD_PROCS=20
-EXEC_NEURON="/vol0004/hp200177/u00690/neuron_kplus/specials/sparc64/special"
+MAXEVAL=3
+NUM_OF_GRANDCHILD_PROCS=8
+EXEC_PROG="/vol0004/hp200177/u00690/neuron_kplus/specials/sparc64/special"
 NEURON_OPTION="{SAVEDIR=${Time}}"
 EXEC_HOC="../al_networkSimulation.hoc"
 DIM_CON_MAT=6
 CON_MAT_FILENAME="../cmaes_input/conMat_6.txt";
 PARAMETER_FILENAME="../cmaes_input/params_pn.txt";
 
+
 # minimal estimate_main
 EXEC_FILE="./estimate_main ${NUM_OF_POP} ${MU} ${NUM_OF_CHILD_PROCS} ${MAXEVAL} ${NUM_OF_GRANDCHILD_PROCS} ${EXEC_NEURON} ${NEURON_OPTION} ${EXEC_HOC} ${DIM_CON_MAT} ${CON_MAT_FILENAME} ${PARAMETER_FILENAME} ${CMAES_DIR}"
 
 # execute job
-mpiexec -np 1  ${EXEC_FILE} 2>&1 | tee stdout_seikai100.log
+mpiexec -np 1 -stdout-proc ./%j/%n -stderr-proc ./%j/%n ${EXEC_FILE}
